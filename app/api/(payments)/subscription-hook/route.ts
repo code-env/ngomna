ca422fd18e731de5f773f20a13310700f3f6bbf1
Romaric Lonfonyuy <78319422+Romaric250@users.noreply.github.com>
2024-10-27 09:20:15 +0000

    staging changes

--- app/api/(payments)/subscription-hook/route.ts
+++ app/api/(payments)/subscription-hook/route.ts
@@ -1,97 +1,35 @@
-import { db } from '@/lib/db';
-import { SubscriptionPlan } from '@prisma/client';
-import { NextResponse } from 'next/server';
+import { NextResponse } from "next/server";
+import { db } from "@/lib/db";
 
-export async function GET(
-  req: Request,
-  { params }: { params: { userId: string } }
-) {
-  const { userId } = params;
-
+export async function GET(req: Request) {
   try {
-    if (!userId) {
-      return new NextResponse('Please provide a user id', { status: 400 });
+    const { paymentdata } = await req.json();
+
+    if (!paymentdata) {
+      return new NextResponse("Unauthorized webhook call", { status: 400 });
     }
 
-    const subscriptionstatus = await checksubscriptionstatus(userId);
+    const createSubscription = await db.subscriptions.create({
+      data: {
+        userId: paymentdata.userId,
+        tx_ref: paymentdata.tx_ref,
+        amountpaid: paymentdata.amountpaid,
+        lastpaymentdate: new Date(paymentdata.lastpaymentdate),
+        nextpaymentdate: new Date(paymentdata.nextpaymentdate),
+        daysfromlastpaycount: 0,
+  
+      }
 
-    return new NextResponse(JSON.stringify(subscriptionstatus), {
-      status: 200,
     });
+
+    if (!createSubscription) {
+      return new NextResponse("Subscription creation failed", { status: 400 });
+    }
+    
+
+    return new NextResponse(JSON.stringify(createSubscription), { status: 200 });
   } catch (error: any) {
     console.log(error.message);
-    return new NextResponse(error.message, { status: 500 });
+    return NextResponse.json({ error: error.message }, { status: 500 });
   }
-}
-
-async function checksubscriptionstatus(userId: string) {
-  const recentsubscription = await db.subscriptions.findFirst({
-    where: {
-      userId: userId,
-    },
-    orderBy: {
-      createdAt: 'desc',
-    },
-  });
-
-  if (!recentsubscription) {
-    const checkfreesubscriptionplan = await db.user.findUnique({
-      where: {
-        id: userId,
-      },
-    });
-
-    if (checkfreesubscriptionplan?.payment === SubscriptionPlan.FREE) {
-      const userCreatedAt = new Date(checkfreesubscriptionplan?.createdAt);
-      const currentDate = new Date();
-
-      const freePlanExpiryDate = new Date(userCreatedAt);
-      freePlanExpiryDate.setDate(freePlanExpiryDate.getDate() + 5);
-
-      console.log('Free plan expiry date', freePlanExpiryDate);
-      if (currentDate.getTime() < freePlanExpiryDate.getTime()) {
-        return {
-          issubscriptionfinished: false,
-          expiringdate: freePlanExpiryDate.toLocaleDateString(),
-        };
-      }
-    }
-
-    console.log('No subscription found');
-
-    const p = await db.user.update({
-      where: {
-        id: userId,
-      },
-      data: {
-        payment: SubscriptionPlan.NOTPAID,
-      },
-    });
-
-    console.log('Updated user', p);
-
-    return { issubscriptionfinished: true, expiringdate: null };
-  }
-
-  const hasCurrentSubscriptionFinished =
-    new Date(recentsubscription.nextpaymentdate).getTime() <
-    new Date().getTime();
-
-  if (hasCurrentSubscriptionFinished) {
-    await db.user.update({
-      where: {
-        id: userId,
-      },
-      data: {
-        payment: SubscriptionPlan.NOTPAID,
-      },
-    });
-  }
-
-  // console.log('Has subscription finished', hasCurrentSubscriptionFinished)
-
-  return {
-    issubscriptionfinished: hasCurrentSubscriptionFinished,
-    expiringdate: recentsubscription.nextpaymentdate.toLocaleDateString(),
-  };
 }
